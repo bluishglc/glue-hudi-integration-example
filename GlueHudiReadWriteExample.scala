09fb791b6171307e987daa63e678e5f174a2f16b
Laurence Geng <bluishglc@126.com>
2021-04-18 10:59:12 +0800

    Add files via upload

---
+++ GlueHudiReadWriteExample.scala
@@ -0,0 +1,182 @@
+package com.github
+
+import com.amazonaws.services.glue.GlueContext
+import com.amazonaws.services.glue.util.{GlueArgParser, Job}
+import org.apache.hudi.config.HoodieWriteConfig
+import org.apache.hudi.hive.NonPartitionedExtractor
+import org.apache.hudi.keygen.NonpartitionedKeyGenerator
+import org.apache.hudi.{DataSourceReadOptions, DataSourceWriteOptions}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.JavaConverters._
+
+/**
+ * An example to demo how Glue read and write hudi dataset, and also sync metadata to Glue Catalog.
+ *
+ * @author Laurence Geng (https://laurence.blog.csdn.net/)
+ */
+object GlueHudiReadWriteExample {
+
+	case class User(id: Long, name: String, age: Int, updatedTime: Long)
+
+	val userTableName = "user"
+	val userRecordKeyField = "id"
+	val userPrecombineField = "updatedTime"
+
+	var bucketName:String = _
+	var userTablePath:String = _
+
+  var spark: SparkSession = _
+
+	def main(sysArgs: Array[String]): Unit = {
+
+		init(sysArgs)
+
+		val sparkImplicits = spark.implicits
+		import sparkImplicits._
+
+		val users1 = Seq(
+			User(1, "Tom", 24, System.currentTimeMillis()),
+			User(2, "Bill", 32, System.currentTimeMillis())
+		)
+		val dataframe1 = users1.toDF
+
+		// save 2 user records into hudi, but won't create table in glue catalog
+		saveUserAsHudiWithoutHiveTableSync(dataframe1)
+
+		val dataframe2 = readUserFromHudi()
+
+		val users2 = dataframe2.as[User].collect().toSeq
+
+		println("printing user records in dataframe2...")
+
+		users2.foreach(println(_))
+
+		val users3 = users2 :+
+			User(2, "Bill", 33, System.currentTimeMillis()) :+
+			User(3, "Rose", 45, System.currentTimeMillis())
+
+		val dataframe3 = users3.toDF
+
+		// append 2 new user records, one is updating Bill's age from 32 to 33,
+		// the other is a new user whose name is 'Rose'. This time, we will enable
+		// hudi hive syncing function, and a table named `user` will be created on
+		// default database, this action is done by hudi automatically based on
+		// the metadata of hudi user dataset.
+		saveUserAsHudiWithHiveTableSync(dataframe3)
+
+		// since a table is created automatically, now, we can query user table immediately.
+		val dataframe4 = spark.sql("select * from user")
+
+		val users4 = dataframe4.as[User].collect().toSeq
+
+		// the printed messages show:
+		// Bill's is updated, Rose's record is inserted, this demoed UPSERT feature of hudi!
+		println("printing user records in dataframe4...")
+
+		users4.foreach(println(_))
+
+		commit()
+	}
+
+	/**
+	 * 1. Parse job params
+	 * 2. Create SparkSession instance with given configs
+	 * 3. Init glue job
+	 *
+	 * @param sysArgs all params passing from main method
+	 */
+	def init(sysArgs: Array[String]): Unit = {
+		val args = GlueArgParser.getResolvedOptions(sysArgs, Seq("JOB_NAME", "bucketName").toArray)
+		bucketName = args("bucketName")
+		println(s"bucketName=$bucketName")
+		userTablePath = s"s3://$bucketName/$userTableName"
+		println(s"userTablePath=$userTablePath")
+		val conf = new SparkConf()
+		conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+		val sparkContext = new SparkContext(conf)
+		val glueContext = new GlueContext(sparkContext)
+		spark = glueContext.getSparkSession
+		Job.init(args("JOB_NAME"), glueContext, args.asJava)
+	}
+
+	/**
+	 * Commit glue job.
+	 */
+	def commit(): Unit = {
+		Job.commit()
+	}
+
+	/**
+	 * Read 
+	 * @return
+	 */
+	def readUserFromHudi(): DataFrame = {
+		spark
+			.read
+			.format("hudi")
+			.option(DataSourceReadOptions.QUERY_TYPE_OPT_KEY, DataSourceReadOptions.QUERY_TYPE_SNAPSHOT_OPT_VAL)
+			.load(userTablePath) // For incremental query, pass in the root/base path of table
+	}
+
+	/**
+	 * Save a user dataframe as hudi dataset, but WON'T SYNC its metadata to glue catalog,
+	 * In other words, no table will be created after saving.
+	 *
+	 * @param dataframe The dataframe to be saved
+	 */
+	def saveUserAsHudiWithoutHiveTableSync(dataframe: DataFrame) = {
+
+		val hudiOptions = Map[String, String](
+			HoodieWriteConfig.TABLE_NAME -> userTableName,
+			DataSourceWriteOptions.OPERATION_OPT_KEY -> DataSourceWriteOptions.UPSERT_OPERATION_OPT_VAL,
+			DataSourceWriteOptions.TABLE_TYPE_OPT_KEY -> DataSourceWriteOptions.COW_TABLE_TYPE_OPT_VAL,
+			DataSourceWriteOptions.RECORDKEY_FIELD_OPT_KEY -> userRecordKeyField,
+			DataSourceWriteOptions.PRECOMBINE_FIELD_OPT_KEY -> userPrecombineField,
+			DataSourceWriteOptions.KEYGENERATOR_CLASS_OPT_KEY -> classOf[NonpartitionedKeyGenerator].getName,
+			DataSourceWriteOptions.HIVE_PARTITION_EXTRACTOR_CLASS_OPT_KEY -> classOf[NonPartitionedExtractor].getName
+		)
+
+		dataframe
+			.write
+			.format("hudi")
+			.options(hudiOptions)
+			.mode(SaveMode.Append)
+			.save(userTablePath)
+	}
+
+	/**
+	 * Save a user dataframe as hudi dataset, but also SYNC its metadata to glue catalog,
+	 * In other words, after saving, a table named `default.user` will be created automatically by hudi hive sync
+	 * tool on Glue Catalog!
+	 *
+	 * @param dataframe The dataframe to be saved
+	 */
+	def saveUserAsHudiWithHiveTableSync(dataframe: DataFrame) = {
+
+		val hudiOptions = Map[String, String](
+			HoodieWriteConfig.TABLE_NAME -> userTableName,
+			DataSourceWriteOptions.OPERATION_OPT_KEY -> DataSourceWriteOptions.UPSERT_OPERATION_OPT_VAL,
+			DataSourceWriteOptions.TABLE_TYPE_OPT_KEY -> DataSourceWriteOptions.COW_TABLE_TYPE_OPT_VAL,
+			DataSourceWriteOptions.RECORDKEY_FIELD_OPT_KEY -> userRecordKeyField,
+			DataSourceWriteOptions.PRECOMBINE_FIELD_OPT_KEY -> userPrecombineField,
+			DataSourceWriteOptions.KEYGENERATOR_CLASS_OPT_KEY -> classOf[NonpartitionedKeyGenerator].getName,
+			DataSourceWriteOptions.HIVE_PARTITION_EXTRACTOR_CLASS_OPT_KEY -> classOf[NonPartitionedExtractor].getName,
+			// Register hudi dataset as hive table (sync meta data)
+			DataSourceWriteOptions.HIVE_SYNC_ENABLED_OPT_KEY -> "true",
+			DataSourceWriteOptions.HIVE_USE_JDBC_OPT_KEY -> "false", // For glue, it is required to disable sync via hive jdbc!
+			DataSourceWriteOptions.HIVE_DATABASE_OPT_KEY -> "default",
+			DataSourceWriteOptions.HIVE_TABLE_OPT_KEY -> userTableName
+		)
+
+		dataframe
+			.write
+			.format("hudi")
+			.options(hudiOptions)
+			.mode(SaveMode.Append)
+			.save(userTablePath)
+	}
+
+
+}
